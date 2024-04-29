6a1a0dd6337151f04789e41f60093462de555bce
Frederic Bec <bec.frederic90@outlook.fr>
2024-04-29 15:58:10 +0200

    Add request into cinema repository

--- src/main/java/fr/fms/dao/CinemaRepository.java
+++ src/main/java/fr/fms/dao/CinemaRepository.java
@@ -1,7 +1,12 @@
 package fr.fms.dao;
 
 import fr.fms.entities.Cinema;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CinemaRepository extends JpaRepository<Cinema, Long> {
+
+    Page<Cinema> findByName(String name, Pageable pageable);
+    Page<Cinema> findByCityId(Long cityId, Pageable pageable);
 }
