0019dd95dcf12f432d9f86c802f7aa466bc4cf9f
Frederic Bec <bec.frederic90@outlook.fr>
2024-04-29 15:18:56 +0200

    Refactor name package fr.fms

--- src/main/java/fr/fms/SpringDistopiaApplication.java
+++ src/main/java/fr/fms/SpringDistopiaApplication.java
@@ -1,4 +1,4 @@
-package fr.fms.SpringDistopia;
+package fr.fms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/fr/fms/SpringDistopiaApplicationTests.java
+++ src/test/java/fr/fms/SpringDistopiaApplicationTests.java
@@ -1,4 +1,4 @@
-package fr.fms.SpringDistopia;
+package fr.fms;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
