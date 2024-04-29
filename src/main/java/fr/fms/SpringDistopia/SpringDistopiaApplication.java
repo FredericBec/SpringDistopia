7fad7df055a3a692ee6f28a137d6b75488ab6e07
Frederic Bec <bec.frederic90@outlook.fr>
2024-04-29 14:32:00 +0200

    first commit

---
+++ src/main/java/fr/fms/SpringDistopia/SpringDistopiaApplication.java
@@ -0,0 +1,13 @@
+package fr.fms.SpringDistopia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDistopiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDistopiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/fms/SpringDistopia/SpringDistopiaApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.fms.SpringDistopia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDistopiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
