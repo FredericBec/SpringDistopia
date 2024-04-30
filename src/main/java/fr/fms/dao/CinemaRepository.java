df7407166882d221861b67f582504a8677f10120
Frederic Bec <bec.frederic90@outlook.fr>
2024-04-30 13:49:33 +0200

    Modify request findByName

--- src/main/java/fr/fms/dao/CinemaRepository.java
+++ src/main/java/fr/fms/dao/CinemaRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CinemaRepository extends JpaRepository<Cinema, Long> {
 
-    Page<Cinema> findByName(String name, Pageable pageable);
+    Page<Cinema> findByNameContains(String name, Pageable pageable);
     Page<Cinema> findByCityId(Long cityId, Pageable pageable);
 }
