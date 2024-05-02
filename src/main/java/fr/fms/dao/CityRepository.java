e6be346ad0a167cbad7a30e698d57e04aa618210
Frederic Bec <bec.frederic90@outlook.fr>
2024-05-02 11:32:58 +0200

    Add request city pageable

--- src/main/java/fr/fms/dao/CityRepository.java
+++ src/main/java/fr/fms/dao/CityRepository.java
@@ -1,7 +1,11 @@
 package fr.fms.dao;
 
 import fr.fms.entities.City;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CityRepository extends JpaRepository<City, Long> {
+
+    Page<City> findByNameContains(String name, Pageable pageable);
 }
