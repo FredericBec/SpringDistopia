e47a30c4a98e79bd2a9d2b8e1f55f4c2c772ddf8
Frederic Bec <bec.frederic90@outlook.fr>
2024-05-02 08:19:42 +0200

    Add request showing list

--- src/main/java/fr/fms/dao/ShowingRepository.java
+++ src/main/java/fr/fms/dao/ShowingRepository.java
@@ -3,5 +3,9 @@ package fr.fms.dao;
 import fr.fms.entities.Showing;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ShowingRepository extends JpaRepository<Showing, Long> {
+
+    List<Showing> findByFilmsId(Long filmId);
 }
